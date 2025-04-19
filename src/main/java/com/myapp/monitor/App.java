f80a1ab69f1a8b5da010dfdc43b1a395060bab77
ghana01 <ghanshyammishra58258@gmail.com>
2025-04-19 14:41:13 +0530

    Initial commit - Maven project setup

---
+++ src/main/java/com/myapp/monitor/App.java
@@ -0,0 +1,14 @@
+package com.myapp.monitor;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Health Monitor App is Running!");
+        // Simulated CPU usage or memory usage can go here
+    }
+}
